f39007ccc8204ad4daadf2a255678bb280a95da7
hasan-naim <hasannaim6246@gmail.com>
2022-11-18 15:29:42 +0600

    card design and delete functionality is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import "./App.css";
-import { Button } from "antd";
-import { useState } from "react";
+import { Button, Col, Row } from "antd";
+import { useEffect, useState } from "react";
 import Loading from "./components/Loading/Loading";
-
+import axios from "axios";
+import CardElem from "./components/Card/Card";
 function App() {
   const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  return <div className="App">{loading ? <Loading /> : <></>}</div>;
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      setUserData([...res.data]);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    });
+  }, []);
+
+  /// delete function
+  const handleDelete = (user) => {
+    const filteredData = userData.filter((us) => us.id !== user.id);
+    setUserData(filteredData);
+  };
+
+  return (
+    <div className="App">
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="container">
+            <Row
+              className="row"
+              gutter={[{ xs: 8, sm: 16, md: 24, lg: 32, xl: 48 }, 16]}
+            >
+              {userData.map((user) => (
+                <CardElem
+                  key={user.id}
+                  user={user}
+                  handleDelete={handleDelete}
+                />
+              ))}
+            </Row>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+  HeartFilled,
+  HeartOutlined,
+  DeleteFilled,
+} from "@ant-design/icons";
+import { MailOutlined, PhoneOutlined, GlobalOutlined } from "@ant-design/icons";
+import { Card, Col } from "antd";
+import "./Card.css";
+import axios, { Axios } from "axios";
+import Parser from "html-react-parser";
 
-function Card() {
-  return <div>Card</div>;
+const { Meta } = Card;
+
+function CardElem({ user, handleDelete }) {
+  const [loved, setLoved] = useState(false);
+  const [userImg, setUserImg] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://avatars.dicebear.com/v2/avataaars/${user?.username}.svg?options[mood][]=happy
+`
+      )
+      .then((res) => {
+        console.log("img", typeof res.data);
+        setUserImg(res.data);
+      });
+  }, [user]);
+
+  console.log("user", user);
+  return (
+    <Col className="gutter-row" xs={24} sm={24} md={12} lg={8} xl={6}>
+      <Card
+        style={{ width: 300 }}
+        cover={
+          <img
+            src={`https://avatars.dicebear.com/v2/avataaars/${user?.username}.svg?options[mood][]=happy`}
+            alt={`${user?.username}`}
+          />
+        }
+        actions={[
+          <>
+            {loved ? (
+              <HeartFilled
+                onClick={() => setLoved(!loved)}
+                className="icon iconHeart"
+              />
+            ) : (
+              <HeartOutlined
+                onClick={() => setLoved(!loved)}
+                className="icon iconHeart"
+              />
+            )}
+          </>,
+          <EditOutlined className="icon" key="edit" />,
+          <>
+            <DeleteFilled className="icon" onClick={() => handleDelete(user)} />
+          </>,
+        ]}
+      >
+        <Meta
+          title={user.name}
+          description={
+            <>
+              <div className="desc-div">
+                <p>
+                  <MailOutlined className="icon iconDesc" /> {user.email}
+                </p>
+                <p>
+                  <PhoneOutlined className="icon iconDesc" /> {user.phone}
+                </p>
+                <p>
+                  <GlobalOutlined className="icon iconDesc" /> {user.website}
+                </p>
+              </div>
+            </>
+          }
+        />
+      </Card>
+    </Col>
+  );
 }
 
-export default Card;
+export default CardElem;
